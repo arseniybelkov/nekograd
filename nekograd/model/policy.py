4895824aeb6c53ae2eea0bb9df1ca4a4d5227cec
arseniybelkov <arsfizteh@gmail.com>
2023-04-14 20:23:46 +0300

    add `Multiply` lr policy; add more lr schedulers tests; fix typo

--- nekograd/model/policy.py
+++ nekograd/model/policy.py
@@ -1,6 +1,5 @@
-from typing import Dict
 from abc import ABC, abstractmethod
-from functools import reduce
+from typing import Dict, Callable
 
 
 class Policy(ABC):
@@ -12,11 +11,12 @@ class Policy(ABC):
         pass
 
 
-class Switch(Policy):
+class Multiply(Policy):
     """
-    Useful with torch.optim.LambdaLR
-    lr_policy = Switch({1: 0.1, 3: 0.1})
-    scheduler = torch.optim.LambdaLR(optimizer, lambda_lr=lr_policy, lr_init=1)
+    Useful with torch.optim.lr_scheduler.LambdaLR
+    lr_policy = Multiply({1: 0.1, 3: 0.1})
+    optimizer = Adam(model.parameters(), lr=lr_init)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_policy)
     Suppose we have 6 epochs
     lr = {0: 1, 1: 0.1, 2: 0.1, 3: 0.01, 4: 0.01, 5: 0.01} # lr2epoch mapping
     """
@@ -31,3 +31,22 @@ class Switch(Policy):
     def epoch2value(self, epoch: int) -> float:
         self.factor *= self.mapping.get(epoch, 1)
         return self.factor
+
+
+class Switch(Policy):
+    """
+    Useful with torch.optim.lr_scheduler.LambdaLR
+    lr_policy = Switch(lr_init, lambda i: np.cos(i / np.pi))
+    optimizer = Adam(model.parameters(), lr=lr_init)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_policy)
+    On i-th epoch lr_i = cos(i / np.pi)
+    """
+    def __init__(self, epoch2lr: Callable[[int], float], lr_init: float):
+        super().__init__()
+        if lr_init == 0:
+            raise ValueError(f"lr_init must not be equal to 0")
+        self.lr_init = lr_init
+        self.epoch2lr = epoch2lr
+
+    def epoch2value(self, epoch: int) -> float:
+        return self.epoch2lr(epoch) / self.lr_init
--- tests/model/test_policy.py
+++ tests/model/test_policy.py
@@ -1,14 +1,42 @@
 import numpy as np
+import torch.nn as nn
+from torch.optim import SGD
+from torch.optim.lr_scheduler import LambdaLR
 
-from nekograd.model.policy import Switch
+from nekograd.model.policy import Switch, Multiply
+
+
+def test_multiply():
+    multiply_policy = Multiply({1: 0.1, 3: 0.1})
+    lr = 2
+    lrs = [multiply_policy(epoch) * lr for epoch in range(5)]
+    assert np.allclose(lrs, [2, 0.2, 0.2, 0.02, 0.02])
+
+    f = nn.Linear(2, 2)
+    optimizer = SGD(f.parameters(), lr=lr)
+    scheduler = LambdaLR(optimizer, lr_lambda=Multiply({1: 0.1, 3: 0.1}))
+    lrs = []
+    for _ in range(5):
+        lrs.extend(scheduler.get_last_lr())
+        optimizer.step()
+        scheduler.step()
+
+    assert np.allclose(lrs, [2, 0.2, 0.2, 0.02, 0.02])
 
 
 def test_switch():
-    switch_policy = Switch({1: 0.1, 3: 0.1})
     lr = 2
-    lrs = [switch_policy(epoch) * lr for epoch in range(5)]
-    assert np.allclose(lrs, [2, 0.2, 0.2, 0.02, 0.02])
+    multiply_policy = Switch(lambda i: np.cos(i / np.pi), lr_init=lr)
+    lrs = [multiply_policy(epoch) * lr for epoch in range(5)]
+    assert np.allclose(lrs, [np.cos(i / np.pi) for i in range(5)])
 
-    switch_policy = Switch({3: 0.1, 1: 0.1})
-    lrs = [switch_policy(epoch) * lr for epoch in range(5)]
-    assert np.allclose(lrs, [2, 0.2, 0.2, 0.02, 0.02])
+    f = nn.Linear(2, 2)
+    optimizer = SGD(f.parameters(), lr=lr)
+    scheduler = LambdaLR(optimizer, lr_lambda=Switch(lambda i: np.cos(i / np.pi), lr_init=lr))
+    lrs = []
+    for _ in range(5):
+        lrs.extend(scheduler.get_last_lr())
+        optimizer.step()
+        scheduler.step()
+
+    assert np.allclose(lrs, [np.cos(i / np.pi) for i in range(5)])
