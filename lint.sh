627a1cec007743f2c74c9df5464d98e7db2b073c
arseniybelkov <arsfizteh@gmail.com>
2023-04-24 18:50:56 +0300

    add removing of unused imports

--- lint.sh
+++ lint.sh
@@ -1,4 +1,6 @@
 scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
 cd "$scriptDir" || exit 1
 
+autoflake --remove-all-unused-imports \
+          --ignore-init-module-imports --in-place -r nekograd
 black nekograd; isort nekograd; flake8 nekograd
