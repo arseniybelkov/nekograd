09d414809787b79ab702518c01053b4ccc27d8fb
arseniybelkov <a.belkov@ira-labs.com>
2023-06-01 04:06:25 +0300

    fix draft TimeProfiler

--- nekograd/model/callbacks.py
+++ nekograd/model/callbacks.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 
 from more_itertools import windowed
 from pytorch_lightning.callbacks import Callback
+from toolz import keymap
 
 
 class TimeProfiler(Callback):
@@ -26,13 +27,12 @@ class TimeProfiler(Callback):
 
     def log_time(self, key: str) -> None:
         if key in self.keys:
-            key = f"{self.__class__.__name__}/" + key
             if key not in self.time_stamps:
                 self.time_stamps[key] = [datetime.now()]
             else:
                 self.time_stamps[key].append(datetime.now())
 
-    def compute_time_delta(self) -> Dict[str, float]:
+    def compute_time_delta(self, *keys: str) -> Dict[str, float]:
         def delta(t1, t2=None):
             if isinstance(t1, (list, tuple)):
                 return (t1[1] - t1[0]).total_seconds()
@@ -41,9 +41,15 @@ class TimeProfiler(Callback):
         deltas = {}
         for key, time_stamps in self.time_stamps.items():
             deltas[key] = list(map(delta, windowed(time_stamps, 2)))
-            deltas[key] = sum(deltas[key]) / len(delta[key])
+            deltas[key] = sum(deltas[key]) / len(deltas[key])
 
         return deltas
+    
+    def log_to_logger(self):
+        deltas = self.compute_time_delta()
+        self.pl_module.log_dict(keymap(lambda k: f"{self.__class__.__name__}/" + k, deltas),
+                           on_step=False, on_epoch=True, prog_bar=False)
+        self.time_stamps.clear()
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
         self.log_time("Avg Train Step")
@@ -57,6 +63,7 @@ class TimeProfiler(Callback):
 
     def on_train_epoch_end(self, trainer, pl_module):
         self.log_time("Train Epoch")
+        self.log_to_logger()
 
     def on_validation_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
         self.log_time("Avg Validation Step")
@@ -79,7 +86,7 @@ class TimeProfiler(Callback):
     def on_before_optimizer_step(self, trainer, pl_module, optimizer):
         self.log_time("Avg Optimizer Step")
 
-    def teardown(self, trainer, pl_module, stage: str):
-        if stage == "validate":
-            pl_module.log_dict(self.compute_time_delta(), on_step=True, on_epoch=True, prog_bar=False)
+    def setup(self, trainer, pl_module, stage: str):
+        if stage == "fit":
             self.time_stamps.clear()
+            self.pl_module = pl_module
