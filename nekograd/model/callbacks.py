dcdec6f0c4e77d6aa16d59e34ea1ba2adebb3bb6
arseniybelkov <arsfizteh@gmail.com>
2023-06-01 20:13:05 +0300

    change logging

--- nekograd/model/callbacks.py
+++ nekograd/model/callbacks.py
@@ -10,14 +10,12 @@ from toolz import keymap
 class TimeProfiler(Callback):
     def __init__(self, *keys: str):
         self._default_keys = (
-            "Train Epoch",
-            "Validation Epoch",
-            "Total Batch Iter",
-            "Avg Batch Iter",
-            "Avg Train Step",
-            "Avg Validation Step"
+            "train batch",
+            "validation batch",
+            "train epoch",
+            "validation epoch",
         )
-        self._optional_keys = ("Avg Backward", "Avg Optimizer Step")
+        self._optional_keys = ("backward", "optimizer step")
 
         allowed_keys = self._default_keys + self._optional_keys
         _keys = sorted(set(keys).intersection(allowed_keys))
@@ -39,50 +37,57 @@ class TimeProfiler(Callback):
 
         deltas = {}
         for key, time_stamps in self.time_stamps.items():
+            if len(time_stamps) % 2 == 1:
+                continue
             deltas[key] = list(map(delta, windowed(time_stamps, 2, step=2)))
             deltas[key] = sum(deltas[key]) / len(deltas[key])
 
         return deltas
 
-    def log_to_logger(self, pl_module):
+    def log_to_logger(self, pl_module, clear: bool = True):
         deltas = self.compute_time_delta()
         pl_module.log_dict(keymap(lambda k: f"{self.__class__.__name__}/" + k, deltas), prog_bar=False)
-        self.time_stamps.clear()
+        if clear:
+            self.time_stamps.clear()
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
-        self.log_time("Avg Train Step")
+        self.log_time("train batch")
 
-    def on_train_batch_end(self, trainer, pl_nodule, outputs, batch, batch_idx):
-        self.log_time("Avg Train Step")
-        self.log_time("Avg Optimizer Step")
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        self.log_time("train batch")
+        self.log_time("optimizer step")
+        self.log_to_logger(pl_module, False)
 
     def on_train_epoch_start(self, trainer, pl_module):
-        self.log_time("Train Epoch")
+        self.log_time("train epoch")
 
     def on_train_epoch_end(self, trainer, pl_module):
-        self.log_time("Train Epoch")
-        self.log_to_logger(pl_module)
+        self.log_time("train epoch")
+        self.log_to_logger(pl_module, True)
 
     def on_validation_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
-        self.log_time("Avg Validation Step")
+        self.log_time("validation batch")
 
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
-        self.log_time("Avg Validation Step")
+        self.log_time("validation batch")
+        self.log_to_logger(pl_module, False)
 
     def on_validation_epoch_start(self, trainer, pl_module):
-        self.log_time("Validation Epoch")
+        self.log_time("validation epoch")
 
     def on_validation_epoch_end(self, trainer, pl_module):
-        self.log_time("Validation Epoch")
+        self.log_time("validation epoch")
+        self.log_to_logger(pl_module, False)
 
     def on_before_backward(self, trainer, pl_module, loss):
-        self.log_time("Avg Backward")
+        self.log_time("backward")
 
     def on_after_backward(self, trainer, pl_module):
-        self.log_time("Avg Backward")
+        self.log_time("backward")
 
     def on_before_optimizer_step(self, trainer, pl_module, optimizer):
-        self.log_time("Avg Optimizer Step")
+        self.log_time("optimizer step")
+        self.log_to_logger(pl_module, False)
 
     def setup(self, trainer, pl_module, stage: str):
         if stage == "fit":
